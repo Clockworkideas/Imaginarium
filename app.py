fb48e43580d3b695ebcd956764d660080bd5db32
dahlej <john.dahle@sngular.team>
2022-06-27 17:43:53 -0500

    First Commit

--- app.py
+++ app.py
@@ -6,6 +6,8 @@ from io import BytesIO
 import time
 from flask import Flask, request, jsonify
 from flask_cors import CORS, cross_origin
+from dalle_model import DalleModel
+dalle_model = None
 
 app = Flask(__name__)
 CORS(app)
@@ -13,13 +15,41 @@ print("Welcome to the Imaginarium Server. This might take up to two minutes.")
 
 parser = argparse.ArgumentParser(description = "Imaginarium app to visualize prompts")
 parser.add_argument("--port", type=int, default=8000, help = "backend port")
-
+parser.add_argument("--model_version", type = parse_arg_dalle_version, default = ModelSize.MINI, help = "Mini, Mega, or Mega_full")
 args = parser.parse_args()
 
 @app.route("/ping", methods=["GET"])
 @cross_origin()
 def ping():
     return "pong"
+
+@app.route("/generate", methods=["POST"])
+@cross_origin()
+def generate_images_api():
+    json_data = request.get_json(force=True)
+    text_prompt = json_data["text"]
+    num_images = json_data["num_images"]
+    generated_imgs = dalle_model.generate_images(text_prompt, num_images)
+
+    returned_generated_images = []
+    if args.save_to_disk:
+        dir_name = os.path.join(args.output_dir,f"{time.strftime('%Y-%m-%d_%H:%M:%S')}_{text_prompt}")
+        Path(dir_name).mkdir(parents=True, exist_ok=True)
+
+    for idx, img in enumerate(generated_imgs):
+        if args.save_to_disk:
+            img.save(os.path.join(dir_name, f'{idx}.{args.img_format}'), format=args.img_format)
+
+        buffered = BytesIO()
+        img.save(buffered, format=args.img_format)
+        img_str = base64.b64encode(buffered.getvalue()).decode("utf-8")
+        returned_generated_images.append(img_str)
+
+    print(f"Created {num_images} images from text prompt [{text_prompt}]")
+
+    response = {'generatedImgs': returned_generated_images,
+                'generatedImgsFormat': args.img_format}
+    return jsonify(response)
 
 
 if __name__ == "__main__":
